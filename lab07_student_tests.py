999764c306f446b60dc6081913289968a226dac8
tnguy018 <tnguy018@gmail.com>
2017-11-19 06:20:50 -0800

    Update and rename lab07_tests.py to lab07_student_tests.py

---
+++ lab07_student_tests.py
@@ -0,0 +1,30 @@
+# tests for lab07, UCSB, CMPSC 8
+
+import pytest
+'''
+	You should write your own test cases using pytest. 
+	Here are some examples. 
+'''
+
+from lab07 import createWordList
+
+def test_createWordList_0():
+  assert False
+
+
+def test_createWordList_1():
+  assert False
+
+from lab07 import canWeMakeIt
+
+def test_canWeMakeIt_0():
+  assert(canWeMakeIt('ape',['p', 'a', 'e']) == True)
+
+def test_canWeMakeIt_1():
+  assert(canWeMakeIt('ape',['p', 'a', 'e', 'l']) == True)
+
+
+#You will write your own test cases. 
+
+if __name__ == '__main__':
+   pytest.main(["lab07_student_tests.py", "--capture=sys"])
